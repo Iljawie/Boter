0501b3ae9a057021329e692a7d5596ff85885b26
Iljawie <59835063+Iljawie@users.noreply.github.com>
2020-02-26 16:24:15 +0100

    woensdag
    woensdag push

--- spel.js
+++ spel.js
@@ -7,12 +7,22 @@ var grid = {};
 var clickedCell;
 var messageDisplay = document.querySelector("#message");
 var messagePlayer = document.querySelector("#player");
+var computer = document.querySelector("#human");
+var resetGame = document.getElementById("restart");
+var setupGame = document.getElementById("setup");
+var player1 = document.getElementById("player1").value;
+var displaySecond = document.getElementById("displaySecond");
+var player2 = document.getElementById("player2").value;
 var cell = document.querySelectorAll("td");
 var playerOneOnMove = true;
+resetGame.addEventListener("click", reset);
+computer.addEventListener("click", toggleComputer);
+
 
 init();
 function init() {
     //    setupGrid();
+    setId();
     spelGrid();
     reset();
 }
@@ -22,18 +32,22 @@ function checkWin() {
     if (grid.a1 === grid.a2 && grid.a2 === grid.a3 && grid.a1 != undefined && grid.a2 != undefined && grid.a3 != undefined ||
         grid.b1 === grid.b2 && grid.b2 === grid.b3 && grid.b1 != undefined && grid.b2 != undefined && grid.b3 != undefined ||
         grid.c1 === grid.c2 && grid.c2 === grid.c3 && grid.c1 != undefined && grid.c2 != undefined && grid.c3 != undefined ||
-        
+
         grid.a1 === grid.b1 && grid.b1 === grid.c1 && grid.a1 != undefined && grid.b1 != undefined && grid.c1 != undefined ||
         grid.a2 === grid.b2 && grid.b2 === grid.c2 && grid.a2 != undefined && grid.b2 != undefined && grid.c2 != undefined ||
         grid.a3 === grid.b3 && grid.b3 === grid.c3 && grid.a3 != undefined && grid.b3 != undefined && grid.c3 != undefined ||
-        
+
         grid.a1 === grid.b2 && grid.b2 === grid.c3 && grid.a1 != undefined && grid.b2 != undefined && grid.c3 != undefined ||
         grid.a3 === grid.b2 && grid.b2 === grid.c1 && grid.a3 != undefined && grid.b2 != undefined && grid.c1 != undefined
     ) {
         console.log("winnaar");
         if (playerOneOnMove) {
             alert("player O has won");
-        } else { alert("player X has won"); }
+            reset();
+        } else {
+            alert("player X has won");
+            reset();
+        }
     } else {
         console.log("ik moet huilen");
     }
@@ -78,37 +92,45 @@ function spelGrid() {
         });
     }
 }
-
-// document.getElementsByTagName("td").addEventListener.onclick=function () { console.log(this) }; ///
-
-// accept move? //
-
 // Reset  //
+
+
 function reset() {
     console.log("reset");
     grid = {};
+    clearGrid();
 }
-// Object.keys(grid).forEach(key => grid[key]=null);
-    // delete grid;
-// grid.length = 0;
-    // grid = 0;
-    // grid = {};
 
-    //pick a new random color from array
-    // pickedColor = pickColor();
-    //change colorDisplay to match picked Color
-    // 	colorDisplay.textContent = pickedColor;
-    // 	resetButton.textContent = "New Colors"
-    // 	messageDisplay.textContent = "";
-    // 	//change colors of squares
-    // 	for(var i = 0; i < squares.length; i++){
-    // 		if(colors[i]){
-    // 			squares[i].style.display = "block"
-    // 			squares[i].style.background = colors[i];
-    // 		} else {
-    // 			squares[i].style.display = "none";
-    // 		}
+function clearGrid() {
+    for (var i = 0; i < cell.length; i++) {
+        cell[i].textContent = "";
+    }
+}
 
-// 	h1.style.background = "steelblue";
-// }
-// move
+// Setup ID
+function setId() {
+    console.log("setUp");
+    toggleComputer;
+}
+
+function toggleComputer() {
+    console.log("toggle");
+    if (displaySecond.style.display == '')
+        displaySecond.style.display = 'none';
+    else
+        displaySecond.style.display = '';
+}
+
+function validateForm() {
+    if (player1 == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+}
+// freas setup, remove forms, state names.
+function setToJS() {
+    document.getElementById("player2").value = "JS Computer player";
+}
+// go to play game
+
